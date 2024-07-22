0acaa2be425066693a12c9ad85f59b48fa249c15
Dvalin21 <dvalin21@gmail.com>
2024-07-22 02:20:34 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-sudo apt-get purge apache* mongodb* pigz* -y
+sudo apt-get purge apache2* -y
+sudo apt-get purge mongodb* -y
 sudo apt-get purge php8.2* -y
+sudo apt-get purge pigz* -y
 sudo apt-get purge filecloud* -y
 sudo apt-get autoremove -y
 sudo rm -rf /var/www/html/
 sudo rm -rf /opt/fcnodejs fcpushservice fcwebdrm fileclouddata
-sudo rm -r /usr/share/keyrings/mongodb-server-6.0.gpg filecloud.gpg
-sudo rm -r /etc/apt/sources.list.d/mongodb-org-6.0.list filecloud.list
+sudo rm -r /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo rm -r /usr/share/keyrings/mongodb-server-6.0.gpg
+sudo rm -r /usr/share/keyrings/filecloud.gpg
+sudo rm -r /etc/apt/sources.list.d/filecloud.list
